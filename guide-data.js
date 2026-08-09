4a661a76538e89faf5e09092c0570251215e684c
Sinx <xinusualz321.1@gmail.com>
2026-08-09 00:32:35 -0600

    another meta description fix (vro2)

--- guide-data.js
+++ guide-data.js
@@ -158,7 +158,7 @@ export const guideData = [
         titleKey: 'stage7.content_wtfl',
         raidKeys: ['stage7.sections.wtfl'],
         meta : {
-            title: { en: "Wilting Tower: Flight Guide"},
+            title: { en: "Hedgyhelper - Wilting Tower: Flight Guide"},
             description: {en: "In-depth guide of Doom Aporia's 4th raid dungeon."},
             image: "images/wtfl1.png"
         },
